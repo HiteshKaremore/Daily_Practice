fa7dd9c02358cd54b27e11c979a02f5e540fa70f
Hitesh Karemore <reachitesh0208@gmail.com>
2026-02-09 08:09:42 +0530

    removed ../test01.js

--- test01.js
+++
@@ -1,8 +0,0 @@
-// for loop that multiplies each element in th array [2,4,6] by 2 and stores the results in a new array 'multipliedNumbers'
-
-let multipliedNumbers = [];
-let numbers = [2, 4, 6];
-
-for (let l = 0; l < numbers.length; l++) {
-  multipliedNumbers.push(numbers[l] * 2);
-}
